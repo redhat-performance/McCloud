974f8e532d8ab942053c27af5629aba449951e0c
akrzos <akrzos@redhat.com>
2018-03-13 07:29:43 -0400

    Several ComputeHCI deployment fixes (Scenarios 8 & 9)

--- roles/001-undercloud-terminal-multiplexers/files/deploy8-tmux.sh
+++ roles/001-undercloud-terminal-multiplexers/files/deploy8-tmux.sh
@@ -2,7 +2,7 @@
 # tmux setup for use with Browbeat - 1 Controller / 6 ComputeHCIs
 SESSION='browbeat'
 ssh_config_home='/home/stack/browbeat/ansible'
-compute_name='overcloud-compute'
+compute_name='overcloud-computehci'
 
 tmux -2 new-session -d -s $SESSION -n 'undercloud-root'
 
@@ -27,70 +27,54 @@ tmux send-keys "cd browbeat/log" C-m
 tmux send-keys "touch debug.log; tailf debug.log" C-m
 tmux select-pane -t 0
 
-# Single Pane Controllers
-tmux new-window -t $SESSIION:4 -n 'controllers'
+# Single Pane Controller
+tmux new-window -t $SESSIION:4 -n 'controller'
 tmux send-keys "su - stack" C-m
 tmux send-keys "cd ${ssh_config_home}" C-m
 tmux send-keys "ssh -F ssh-config overcloud-controller-0" C-m
 tmux send-keys "sudo su -" C-m
-tmux split-window -v
-tmux select-pane -t 1
-tmux send-keys "su - stack" C-m
-tmux send-keys "cd ${ssh_config_home}" C-m
-tmux send-keys "ssh -F ssh-config overcloud-controller-1" C-m
-tmux send-keys "sudo su -" C-m
-tmux split-window -h
-tmux select-pane -t 2
-tmux send-keys "su - stack" C-m
-tmux send-keys "cd ${ssh_config_home}" C-m
-tmux send-keys "ssh -F ssh-config overcloud-controller-2" C-m
-tmux send-keys "sudo su -" C-m
-tmux select-pane -t 0
-
-# Single Pane Networker
-tmux new-window -t $SESSIION:5 -n 'networker'
-tmux send-keys "su - stack" C-m
-tmux send-keys "cd ${ssh_config_home}" C-m
-tmux send-keys "ssh -F ssh-config overcloud-networker-0" C-m
-tmux send-keys "sudo su -" C-m
 tmux select-pane -t 0
 
 # Single Pane Computes
-tmux new-window -t $SESSIION:6 -n 'computes0-2'
+tmux new-window -t $SESSIION:5 -n 'Computes0-3'
 tmux send-keys "su - stack" C-m
 tmux send-keys "cd ${ssh_config_home}" C-m
 tmux send-keys "ssh -F ssh-config ${compute_name}-0" C-m
 tmux send-keys "sudo su -" C-m
 tmux split-window -v
+tmux select-pane -t 0
+tmux split-window -h
 tmux select-pane -t 1
 tmux send-keys "su - stack" C-m
 tmux send-keys "cd ${ssh_config_home}" C-m
 tmux send-keys "ssh -F ssh-config ${compute_name}-1" C-m
 tmux send-keys "sudo su -" C-m
-tmux split-window -h
 tmux select-pane -t 2
 tmux send-keys "su - stack" C-m
 tmux send-keys "cd ${ssh_config_home}" C-m
 tmux send-keys "ssh -F ssh-config ${compute_name}-2" C-m
 tmux send-keys "sudo su -" C-m
+tmux split-window -h
+tmux select-pane -t 3
+tmux send-keys "su - stack" C-m
+tmux send-keys "cd ${ssh_config_home}" C-m
+tmux send-keys "ssh -F ssh-config ${compute_name}-3" C-m
+tmux send-keys "sudo su -" C-m
 tmux select-pane -t 0
 
-# Controller/Window:
-tmux new-window -t $SESSIION:7 -n 'controller0'
+# Single Pane Computes4-5
+tmux new-window -t $SESSIION:6 -n 'computes4-5'
 tmux send-keys "su - stack" C-m
 tmux send-keys "cd ${ssh_config_home}" C-m
-tmux send-keys "ssh -F ssh-config overcloud-controller-0" C-m
+tmux send-keys "ssh -F ssh-config ${compute_name}-4" C-m
 tmux send-keys "sudo su -" C-m
-tmux new-window -t $SESSIION:8 -n 'controller1'
+tmux split-window -v
+tmux select-pane -t 1
 tmux send-keys "su - stack" C-m
 tmux send-keys "cd ${ssh_config_home}" C-m
-tmux send-keys "ssh -F ssh-config overcloud-controller-1" C-m
+tmux send-keys "ssh -F ssh-config ${compute_name}-5" C-m
 tmux send-keys "sudo su -" C-m
-tmux new-window -t $SESSIION:9 -n 'controller2'
-tmux send-keys "su - stack" C-m
-tmux send-keys "cd ${ssh_config_home}" C-m
-tmux send-keys "ssh -F ssh-config overcloud-controller-2" C-m
-tmux send-keys "sudo su -" C-m
+tmux select-pane -t 0
 
 tmux select-window -t $SESSION:2
 
--- roles/001-undercloud-terminal-multiplexers/files/deploy9-tmux.sh
+++ roles/001-undercloud-terminal-multiplexers/files/deploy9-tmux.sh
@@ -2,7 +2,7 @@
 # tmux setup for use with Browbeat - 3 Controllers / 4 ComputeHCIs
 SESSION='browbeat'
 ssh_config_home='/home/stack/browbeat/ansible'
-compute_name='overcloud-compute'
+compute_name='overcloud-computehci'
 
 tmux -2 new-session -d -s $SESSION -n 'undercloud-root'
 
@@ -47,46 +47,45 @@ tmux send-keys "ssh -F ssh-config overcloud-controller-2" C-m
 tmux send-keys "sudo su -" C-m
 tmux select-pane -t 0
 
-# Single Pane Networker
-tmux new-window -t $SESSIION:5 -n 'networker'
-tmux send-keys "su - stack" C-m
-tmux send-keys "cd ${ssh_config_home}" C-m
-tmux send-keys "ssh -F ssh-config overcloud-networker-0" C-m
-tmux send-keys "sudo su -" C-m
-tmux select-pane -t 0
-
 # Single Pane Computes
-tmux new-window -t $SESSIION:6 -n 'computes0-2'
+tmux new-window -t $SESSIION:5 -n 'Computes0-3'
 tmux send-keys "su - stack" C-m
 tmux send-keys "cd ${ssh_config_home}" C-m
 tmux send-keys "ssh -F ssh-config ${compute_name}-0" C-m
 tmux send-keys "sudo su -" C-m
 tmux split-window -v
+tmux select-pane -t 0
+tmux split-window -h
 tmux select-pane -t 1
 tmux send-keys "su - stack" C-m
 tmux send-keys "cd ${ssh_config_home}" C-m
 tmux send-keys "ssh -F ssh-config ${compute_name}-1" C-m
 tmux send-keys "sudo su -" C-m
-tmux split-window -h
 tmux select-pane -t 2
 tmux send-keys "su - stack" C-m
 tmux send-keys "cd ${ssh_config_home}" C-m
 tmux send-keys "ssh -F ssh-config ${compute_name}-2" C-m
 tmux send-keys "sudo su -" C-m
+tmux split-window -h
+tmux select-pane -t 3
+tmux send-keys "su - stack" C-m
+tmux send-keys "cd ${ssh_config_home}" C-m
+tmux send-keys "ssh -F ssh-config ${compute_name}-3" C-m
+tmux send-keys "sudo su -" C-m
 tmux select-pane -t 0
 
 # Controller/Window:
-tmux new-window -t $SESSIION:7 -n 'controller0'
+tmux new-window -t $SESSIION:6 -n 'controller0'
 tmux send-keys "su - stack" C-m
 tmux send-keys "cd ${ssh_config_home}" C-m
 tmux send-keys "ssh -F ssh-config overcloud-controller-0" C-m
 tmux send-keys "sudo su -" C-m
-tmux new-window -t $SESSIION:8 -n 'controller1'
+tmux new-window -t $SESSIION:7 -n 'controller1'
 tmux send-keys "su - stack" C-m
 tmux send-keys "cd ${ssh_config_home}" C-m
 tmux send-keys "ssh -F ssh-config overcloud-controller-1" C-m
 tmux send-keys "sudo su -" C-m
-tmux new-window -t $SESSIION:9 -n 'controller2'
+tmux new-window -t $SESSIION:8 -n 'controller2'
 tmux send-keys "su - stack" C-m
 tmux send-keys "cd ${ssh_config_home}" C-m
 tmux send-keys "ssh -F ssh-config overcloud-controller-2" C-m
